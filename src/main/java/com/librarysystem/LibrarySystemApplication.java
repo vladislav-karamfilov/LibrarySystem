c0f23b24d5b5d826a889766d3588692278442322
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2016-01-25 21:29:16 +0200

    Created the initial project structure.

---
+++ src/main/java/com/librarysystem/LibrarySystemApplication.java
@@ -0,0 +1,13 @@
+package com.librarysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class LibrarySystemApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarySystemApplication.class, args);
+	}
+}
